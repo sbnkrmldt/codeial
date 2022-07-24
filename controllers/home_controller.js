bde356f3f5b246138825b460a5b133227b141db6
Subhankar <subhankarmldt@gmail.com>
2022-07-24 19:03:39 +0530

    setup static files access for layouts and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 module.exports.home = function(req , res){
-    return res.end('<h1> Express is up for Codeiel</h1>')
+    return res.render('home',{
+        title:"Home"
+    });
 }
 // module.exports.actionName = function(req,res){}
